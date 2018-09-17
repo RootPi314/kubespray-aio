78bf966820447796ffc3f05a06f24e306356f0db
Maxime Guyot <Miouge1@users.noreply.github.com>
2018-09-17 14:39:26 +0200

    Fix pip command

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 sudo apt-get update
 sudo apt-get install -y python-pip python-netaddr
-sudo pip  --upgrade pip ansible
+sudo pip install --upgrade pip ansible
 git clone https://github.com/kubernetes-incubator/kubespray.git && cd kubespray
 git checkout v2.4.0 && cd ..
 ansible-playbook -i hosts.ini kubespray/cluster.yml
